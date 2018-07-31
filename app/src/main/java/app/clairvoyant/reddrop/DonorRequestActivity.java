0a5deb2cef5c712d27937667175812c3c5dbd8f3
redviper2017 <tanzeerhossain@zoho.com>
2018-07-31 00:13:59 -0700

    Second commit

--- app/src/main/java/app/clairvoyant/reddrop/DonorRequestActivity.java
+++ app/src/main/java/app/clairvoyant/reddrop/DonorRequestActivity.java
@@ -1,18 +1,30 @@
 package app.clairvoyant.reddrop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import app.clairvoyant.reddrop.model.Location;
+import app.clairvoyant.reddrop.network.RetrofitClientInstance;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class DonorRequestActivity extends AppCompatActivity {
 
     private EditText name, number, location;
     private Spinner units, group;
+    private Button donorSearchButton;
+
+    private static final String TAG = "Donor Request Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,8 @@ public class DonorRequestActivity extends AppCompatActivity {
 
         units = findViewById(R.id.spinner_unitsRequired);
         group = findViewById(R.id.spinner_bloodGroup);
+
+        donorSearchButton = findViewById(R.id.btn_search_donor);
 
         // units Spinner Drop down elements
         List<String> unitsRequired = new ArrayList<String>();
@@ -63,5 +77,38 @@ public class DonorRequestActivity extends AppCompatActivity {
         units.setAdapter(unitsDataAdapter);
 
         group.setAdapter(groupsDataAdapter);
+
+        // On click of Search Button
+        donorSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String address = location.getText().toString().trim();
+                Log.d(TAG,"requested donation's address: "+address);
+
+                // Create handle for the RetrofitInstance interface
+                GetLocationDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetLocationDataService.class);
+
+
+                // Call the method with parameter in the interface to get the notice data
+                Call<Location> call = service.getLocationData(address);
+
+                // Log of the URL called
+                Log.d(TAG,"URL called: " + call.request().url());
+
+                call.enqueue(new Callback<Location>() {
+                    @Override
+                    public void onResponse(Call<Location> call, Response<Location> response) {
+                        Log.d(TAG,"recieved response from url is: "+response);
+                        String lat = response.body().getLat().toString();
+                        Log.d(TAG,"latitude is: "+lat);
+                    }
+
+                    @Override
+                    public void onFailure(Call<Location> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
     }
 }
---
+++ app/src/main/java/app/clairvoyant/reddrop/network/RetrofitClientInstance.java
@@ -0,0 +1,19 @@
+package app.clairvoyant.reddrop.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://maps.googleapis.com/maps/api/geocode/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
